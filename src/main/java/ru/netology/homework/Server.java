f72588677ec5496b1216212778a9f11c8bcea56e
3ay <lefrost.volkoff@yandex.ru>
2023-08-16 15:39:27 +0300

    init

---
+++ src/main/java/ru/netology/homework/Server.java
@@ -0,0 +1,99 @@
+package ru.netology.homework;
+
+import java.io.*;
+import java.net.*;
+import java.nio.file.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final List<String> validPaths;
+    private final ExecutorService threadPool;
+
+    public Server(List<String> validPaths) {
+        this.validPaths = validPaths;
+        this.threadPool = Executors.newFixedThreadPool(64);
+    }
+
+    public void start(int port) {
+        try (var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final var socket = serverSocket.accept();
+                threadPool.execute(() -> handleConnection(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleConnection(Socket socket) {
+        try (
+                var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                var out = new BufferedOutputStream(socket.getOutputStream())
+        ) {
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                return;
+            }
+
+            final var path = parts[1];
+            if (!validPaths.contains(path)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                return;
+            }
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+
+            if (path.equals("/classic.html")) {
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+                return;
+            }
+
+            final var length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        final var validPaths = List.of(
+                "/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css",
+                "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js"
+        );
+        Server server = new Server(validPaths);
+        server.start(9999);
+    }
+}
